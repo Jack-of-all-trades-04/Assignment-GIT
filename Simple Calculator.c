81134be439d9730c40951898f89488731c5b28c6
Ferdyano01 <98732008+Ferdyano01@users.noreply.github.com>
2025-05-14 19:25:59 +0700

    Update Simple Calculator.c

--- Simple Calculator.c
+++ Simple Calculator.c
@@ -23,6 +23,7 @@ int main () {
 			printf ("Hasil %26s: %.2lf"," ", add(angkaPertama, angkaKedua));
 			break;
 		case 2 :
+			printf ("Hasil %26s: %.2lf"," ", mul(angkaPertama, angkaKedua));
 			break;
 		case 3 :
 			break;
