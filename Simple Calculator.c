3592bac249378e58f72ec70efbc6e5edcfb3bdfa
Jack-of-all-trades-04 <novanagung08@gmail.com>
2025-05-14 14:44:08 +0700

    Sudah ada fungsi ADD dan tampilan ke user
    Perlu fungsi SUB, MUL dan DIV lagi supaya jadi kalkulator sederhana utuh.

--- Simple Calculator.c
+++ Simple Calculator.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 
+double add(double angkaPertama, double angkaKedua) {
+	return angkaPertama+angkaKedua;
+}
+
 int main () {
+	double angkaPertama, angkaKedua;
+	int input = 0;
 	
+	puts ("Selamat datang di kalkulator sederhana. \nSilahkan pilih operasi yang tersedia :\n1. Tambah\n2. Kurang\n3. Kali\n4. Bagi");
+	while (input <= 0 || input > 4) {
+		printf ("Silahkan pilih operasi : ");
+		scanf ("%d", &input);
+		if (input <= 0 || input > 4) puts ("Pilihan tidak valid, coba lagi.\n");
+	}
+	printf ("\nSilahkan masukkan angka pertama\t: ");
+	scanf ("%lf", &angkaPertama);
+	printf ("Silahkan masukkan angka kedua\t: ");
+	scanf ("%lf", &angkaKedua);
+	switch (input) {
+		case 1 :
+			printf ("Hasil %26s: %.2lf"," ", add(angkaPertama, angkaKedua));
+			break;
+		case 2 :
+			break;
+		case 3 :
+			break;
+		case 4 :
+			break;
+	}
 	return 0;
 }
